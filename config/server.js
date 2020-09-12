053a7cf9a78235cd96d3005eb7be843804365720
nicovernot <nicovernot@gmail.com>
2020-09-12 20:02:35 +0200

    modif messages

--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cors: {
+   origin : "*"
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1329e66934feef917d7023e5fce72ab3'),

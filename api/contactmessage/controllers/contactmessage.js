a14e7e30a0dc06ebbd9162c69a58a4fd7dea7002
nicovernot <nicovernot@gmail.com>
2020-09-17 10:56:18 +0200

    upd strapi

--- api/contactmessage/controllers/contactmessage.js
+++ api/contactmessage/controllers/contactmessage.js
@@ -5,4 +5,27 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.adresse.create(data, { files });
+        } else {
+          entity = await strapi.services.adresse.create(ctx.request.body);
+        }
+         console.log(ctx.request.body)
+        await strapi.plugins['email'].services.email.send({
+            to: ctx.request.body.mail,
+            from: 'niveco13380@gmail.com',
+            subject: `Merci ${ctx.request.body.nom} de nous avoir contacté` ,
+            text: `
+             Nos vous contacterons prochainement .
+    
+              Cordialement,
+              l\'equipe Redma
+            `,
+          });
+  
+    }
+};
